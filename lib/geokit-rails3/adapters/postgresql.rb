73ece49719b3e5c3b20bb9f421a6cb1c019e16a3
phereford <phereford@gmail.com>
2013-02-10 18:21:35 -0500

    a

--- lib/geokit-rails3/adapters/postgresql.rb
+++ lib/geokit-rails3/adapters/postgresql.rb
@@ -4,9 +4,7 @@ module Geokit
       
       def sphere_distance_sql(lat, lng, multiplier)
         %|
-          (ACOS(least(1,COS(#{lat})*COS(#{lng})*COS(RADIANS(#{qualified_lat_column_name}))*COS(RADIANS(#{qualified_lng_column_name}))+
-          COS(#{lat})*SIN(#{lng})*COS(RADIANS(#{qualified_lat_column_name}))*SIN(RADIANS(#{qualified_lng_column_name}))+
-          SIN(#{lat})*SIN(RADIANS(#{qualified_lat_column_name}))))*#{multiplier})
+          (ACOS(least(1,COS(#{lat})*COS(#{lng})*COS(RADIANS(#{qualified_lat_column_name}))*COS(RADIANS(#{qualified_lng_column_name}))+COS(#{lat})*SIN(#{lng})*COS(RADIANS(#{qualified_lat_column_name}))*SIN(RADIANS(#{qualified_lng_column_name}))+SIN(#{lat})*SIN(RADIANS(#{qualified_lat_column_name}))))*#{multiplier})
          |
       end
       
